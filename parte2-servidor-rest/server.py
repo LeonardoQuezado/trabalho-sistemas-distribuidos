5929ab5711c76673686de09406aab9fab92e66eb
Leonardo Quezado <leoquezado@outlook.com>
2025-12-07 12:34:42 -0300

    Adiciona documentação Swagger e corrige OpenAPI para 3.0.0

--- parte2-servidor-rest/server.py
+++ parte2-servidor-rest/server.py
@@ -152,6 +152,34 @@ def index():
             "DELETE /favorites/<id>": "Remove favorito"
         }
     })
+from flask import send_file
 
+@app.route('/openapi.yaml')
+def get_openapi():
+    """Retorna especificação OpenAPI"""
+    return send_file('openapi.yaml', mimetype='text/yaml')
+
+@app.route('/docs')
+def docs():
+    """Redireciona para Swagger UI"""
+    return '''
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>API Documentation</title>
+        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css" />
+    </head>
+    <body>
+        <div id="swagger-ui"></div>
+        <script src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"></script>
+        <script>
+            SwaggerUIBundle({
+                url: '/openapi.yaml',
+                dom_id: '#swagger-ui',
+            });
+        </script>
+    </body>
+    </html>
+    '''
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
